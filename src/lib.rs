0901f9f1b6079f78e2b3dca109d76cd46e31bec5
shane <moloney.shane@yahoo.com>
2020-01-26 16:59:28 +0000

    refactored formatting

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,6 @@ use std::fmt;
 impl Clock {
 
     pub fn new(hours: i32, minutes: i32) -> Self {
-
         Clock{hours, minutes}
     }
     pub fn add_minutes(&self, minutes: i32) -> Self {
@@ -15,11 +14,8 @@ impl Clock {
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        if self.hours == 24 {
-            write!(f, "{:02}:{:02}", 0, self.minutes)
-        }
-       else {write!(f, "{:02}:{:02}", self.hours, self.minutes)}
-    }
+                write!(f, "{:02}:{:02}", (self.hours%24)+self.minutes/60, self.minutes%60)
+            }
 }
 
 
